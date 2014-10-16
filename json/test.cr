f79e85daa710f68e08d5aca2abf9667fef8001fa
Konstantin Makarchev <kostya27@gmail.com>
2014-10-16 19:40:29 +0400

    fix

--- json/test.cr
+++ json/test.cr
@@ -2,4 +2,17 @@ require "json"
 
 text = File.read("1.json")
 json = Json.parse(text) as Hash
-puts json.length
+coordinates = json["coordinates"] as Array
+res = coordinates.inject({:x => 0.0, :y => 0.0, :z => 0.0}) do |r, e|
+  h = e as Hash
+  r[:x] += h["x"] as Float64
+  r[:y] += h["y"] as Float64
+  r[:z] += h["z"] as Float64
+  r
+end
+
+p coordinates.length
+p res
+p res[:x] / coordinates.length
+p res[:y] / coordinates.length
+p res[:z] / coordinates.length
