38485bb8ad775f957aafc9933aeabc33896298db
Martin Nowak <code@dawg.eu>
2015-03-31 20:58:11 +0200

    improve D settings (use inline)

--- brainfuck/build.sh
+++ brainfuck/build.sh
@@ -3,6 +3,6 @@ go build -o brainfuck_go brainfuck.go
 g++ -O3 -o brainfuck_cpp brainfuck.cpp
 scalac -optimize brainfuck.scala
 rustc --opt-level 3 brainfuck.rs -o brainfuck_rs
-dmd -ofbrainfuck_d -O -release brainfuck.d
+dmd -ofbrainfuck_d -O -release -inline brainfuck.d
 nim c -o:brainfuck_nim_clang -d:release --cc:clang --verbosity:0 brainfuck.nim
 nim c -o:brainfuck_nim_gcc -d:release --cc:gcc --verbosity:0 brainfuck.nim
