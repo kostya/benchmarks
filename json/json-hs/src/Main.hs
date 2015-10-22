191ffe7985e82eb46e64915b61a434048e5c405d
Kostiantyn Rybnikov <k-bx@k-bx.com>
2015-10-22 17:36:09 +0300

    json-stream version

--- json/json-hs/src/Main.hs
+++ json/json-hs/src/Main.hs
@@ -1,32 +1,30 @@
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
-import qualified Data.Aeson           as J
-import qualified Data.ByteString.Lazy as BL
-import           Data.Foldable        (foldl')
+import qualified Data.Aeson             as J
+import qualified Data.ByteString.Lazy   as BL
+import           Data.JsonStream.Parser
+import           Data.List              (foldl')
 import           GHC.Generics
 
-newtype Structure = Structure { coordinates :: [Coordinate] }
-    deriving (Generic)
-instance J.FromJSON Structure
 data Coordinate = Coordinate { x :: !Double
                              , y :: !Double
-                             , z :: !Double }
-    deriving (Generic)
+                             , z :: !Double }  deriving (Generic)
 instance J.FromJSON Coordinate
 
 data Res = Res !Double !Double !Double !Int
 
 main :: IO ()
 main = do
-    b <- BL.readFile "1.json"
-    let struct = either error id (J.eitherDecode b)
-    let res = foldl' (\(Res xsum ysum zsum count) (Coordinate{..}) ->
-                          Res (xsum + x) (ysum + y) (zsum + z) (count + 1))
-                     (Res 0 0 0 0)
-                     (coordinates struct)
-    let Res xsum ysum zsum count = res
+    f <- BL.readFile "1.json"
+    let Res xsum ysum zsum count = foldl' op  (Res 0 0 0 0)
+                                 $ parseLazyByteString parser_coordinates f
     let c = fromIntegral count
     print (xsum / c)
     print (ysum / c)
     print (zsum / c)
+ where
+  parser_coordinates = "coordinates" .: arrayOf coord
+  coord = Coordinate <$> ("x" .: real) <*> ("y" .: real) <*> ("z" .: real)
+  op (Res xsum ysum zsum count) (Coordinate{..}) = Res (xsum + x) (ysum + y) (zsum + z) (count + 1)
