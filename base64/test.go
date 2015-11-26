726332fe2f9033ba219c26bbb4c8eb2f796240b1
snadrus <snadrus@gmail.com>
2015-11-25 21:44:19 -0800

    Update test.go

--- base64/test.go
+++ base64/test.go
@@ -9,26 +9,26 @@ func main() {
 	STR_SIZE := 10000000
 	TRIES := 100
 
-	str := strings.Repeat("a", STR_SIZE)
 	str2 := ""
-	bytes := []byte(str)
+	bytes := []byte(strings.Repeat("a", STR_SIZE))
 
 	coder := base64.StdEncoding
 
 	t := time.Now()
-	s := uint64(0)
+	s := 0
 
 	for i := 0; i < TRIES; i += 1 {
 		str2 = coder.EncodeToString(bytes)
-		s += uint64(len(str2))
+		s += len(str2)
 	}
-	fmt.Printf("encode: %d, %.4f\n", s, float32(time.Since(t).Seconds()))
+	fmt.Printf("encode: %d, %.4f\n", s, time.Since(t).Seconds())
 
 	t = time.Now()
 	s = 0
+	var str3 []byte
 	for i := 0; i < TRIES; i += 1 {
-		str3, _ := coder.DecodeString(str2)
-		s += uint64(len(str3))
+		str3, _ = coder.DecodeString(str2)
+		s += len(str3)
 	}
-	fmt.Printf("decode: %d, %.4f\n", s, float32(time.Since(t).Seconds()))
+	fmt.Printf("decode: %d, %.4f\n", s, time.Since(t).Seconds())
 }
