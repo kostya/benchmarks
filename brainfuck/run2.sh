d0711e49e71471e128a22c4aba2c6399dcd22576
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-01-08 14:18:53 +0200

    Include Tcl in mandel.b benchmark

--- brainfuck/run2.sh
+++ brainfuck/run2.sh
@@ -28,6 +28,8 @@ echo Mono
 ../xtime.rb mono -O=all --gc=sgen brainfuck.exe mandel.b > /dev/null
 echo Felix
 ../xtime.rb ./brainfuck_flx mandel.b > /dev/null
+echo Tcl
+../xtime.rb tclsh brainfuck.tcl mandel.b > /dev/null
 echo Java
 ../xtime.rb java brainfuck mandel.b > /dev/null
 
