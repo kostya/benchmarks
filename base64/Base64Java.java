fddc04b5ae3d939d1e015fc3306dd5ba6880cd3c
Konstantin Makarchev <kostya27@gmail.com>
2016-01-26 18:42:40 +0400

    Revert "jit-warming-up removed"
    This reverts commit 0784c0ad1f862b41aadaa7caaaa966daf5192b05.

--- base64/Base64Java.java
+++ base64/Base64Java.java
@@ -20,6 +20,11 @@ class Base64Java {
         String str2 = "";
         String str3;
 
+        // cheat - JIT warming-up  - 0,5-1sec
+        for (int i = 0 ; i < TRIES ; i++) {
+            enc.encodeToString(str.getBytes(ISO_8859_1)).length();
+        }
+
         int s = 0;
         Long t = System.nanoTime();
         for (int i = 0 ; i < TRIES ; i++) {
@@ -27,6 +32,11 @@ class Base64Java {
             s += str2.length();
         }
         out.println("encode: " + s + ", " + (System.nanoTime() - t)/1e9);
+
+        // cheat - JIT warming-up  - 0-0,3sec
+        for (int i = 0 ; i < TRIES ; i++) {
+            dec.decode(str2.getBytes(ISO_8859_1));
+        }
 
         s = 0;
         t = System.nanoTime();
