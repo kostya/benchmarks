fa9d3786cf9fcd7717ec85692d7ad4beacd85b2c
Jack Stouffer <jack@jackstouffer.com>
2016-06-17 11:43:04 -0400

    Use offical D style

--- brainfuck2/bf.d
+++ brainfuck2/bf.d
@@ -6,88 +6,164 @@ import std.conv;
 
 final:
 
-enum OpT { INC, MOVE, PRINT, LOOP };
-
-struct Op {
-  OpT op;
-  int v;
-  Op[] loop;
-
-  this(OpT t, int _v) { op = t; v = _v; }
-  this(OpT t, Op[] _l) { op = t; loop = _l; }
+enum OpT
+{
+    INC,
+    MOVE,
+    PRINT,
+    LOOP
 };
 
-class StringIterator {
-  string text;
-  int pos;
-  this(string t) { text = t; pos = 0; }
-  char next() { return (pos < text.length) ? text[pos++] : 0.to!char; }
+struct Op
+{
+    OpT op;
+    int v;
+    Op[] loop;
+
+    this(OpT t, int _v)
+    {
+        op = t;
+        v = _v;
+    }
+
+    this(OpT t, Op[] _l)
+    {
+        op = t;
+        loop = _l;
+    }
 };
 
-class Tape {
-  int pos;
-  int[] tape;
+class StringIterator
+{
+    string text;
+    int pos;
 
-  this() {
-    pos = 0;
-    tape ~= 0;
-  }
+    this(string t)
+    {
+        text = t;
+        pos = 0;
+    }
+
+    char next()
+    {
+        return (pos < text.length) ? text[pos++] : 0.to!char;
+    }
+};
+
+class Tape
+{
+    int pos;
+    int[] tape;
+
+    this()
+    {
+        pos = 0;
+        tape ~= 0;
+    }
 
 final:
-  int get() { return tape[pos]; }
-  void inc(int x) { tape[pos] += x; }
-  void move(int x) { pos += x; while (pos >= tape.length) tape ~= 0; }
-};
-
-class Program {
-  Op[] ops;
-
-  this(string code) {
-    ops = parse(new StringIterator(code));
-  }
-
-  void run() {
-    _run(ops, new Tape());
-  }
-
-  void _run(Op[] program, Tape tape) {
-    foreach (op; program) {
-      switch (op.op) {
-        case OpT.INC: tape.inc(op.v); break;
-        case OpT.MOVE: tape.move(op.v); break;
-        case OpT.LOOP: while (tape.get() != 0) _run(op.loop, tape); break;
-        case OpT.PRINT: write(tape.get().to!char); stdout.flush(); break;
-        default: break;
-      }
-    }
-  }
-
-  Op[] parse(StringIterator it) {
-    Op[] res;
-
-    while (true) {
-      char c = it.next();
-      if (c.to!int == 0) break;
-
-      switch(c) {
-        case '+': res ~= Op(OpT.INC, 1); break;
-        case '-': res ~= Op(OpT.INC, -1); break;
-        case '>': res ~= Op(OpT.MOVE, 1); break;
-        case '<': res ~= Op(OpT.MOVE, -1); break;
-        case '.': res ~= Op(OpT.PRINT, 0); break;
-        case '[': res ~= Op(OpT.LOOP, parse(it)); break;
-        case ']': return res;
-        default: break;
-      }
+    int get()
+    {
+        return tape[pos];
     }
 
-    return res;
-  }
+    void inc(int x)
+    {
+        tape[pos] += x;
+    }
+
+    void move(int x)
+    {
+        pos += x;
+        while (pos >= tape.length)
+            tape ~= 0;
+    }
 };
 
+class Program
+{
+    Op[] ops;
 
-int main(string[] args){
-  string text = readText(args[1]);
-  new Program(text).run();
-  return 0;
+    this(string code)
+    {
+        ops = parse(new StringIterator(code));
+    }
+
+    void run()
+    {
+        _run(ops, new Tape());
+    }
+
+    void _run(Op[] program, Tape tape)
+    {
+        foreach (op; program)
+        {
+            switch (op.op)
+            {
+            case OpT.INC:
+                tape.inc(op.v);
+                break;
+            case OpT.MOVE:
+                tape.move(op.v);
+                break;
+            case OpT.LOOP:
+                while (tape.get() != 0)
+                    _run(op.loop, tape);
+                break;
+            case OpT.PRINT:
+                write(tape.get().to!char);
+                stdout.flush();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    Op[] parse(StringIterator it)
+    {
+        Op[] res;
+
+        while (true)
+        {
+            char c = it.next();
+            if (c.to!int == 0)
+                break;
+
+            switch (c)
+            {
+            case '+':
+                res ~= Op(OpT.INC, 1);
+                break;
+            case '-':
+                res ~= Op(OpT.INC, -1);
+                break;
+            case '>':
+                res ~= Op(OpT.MOVE, 1);
+                break;
+            case '<':
+                res ~= Op(OpT.MOVE, -1);
+                break;
+            case '.':
+                res ~= Op(OpT.PRINT, 0);
+                break;
+            case '[':
+                res ~= Op(OpT.LOOP, parse(it));
+                break;
+            case ']':
+                return res;
+            default:
+                break;
+            }
+        }
+
+        return res;
+    }
+};
+
+int main(string[] args)
+{
+    string text = readText(args[1]);
+    new Program(text).run();
+    return 0;
 }
