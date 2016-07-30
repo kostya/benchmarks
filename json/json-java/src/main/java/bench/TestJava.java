e8e2b437c929bc4a1417cf9c3754a9661644c4a6
Rikard Pavelic <rikard@ngs.hr>
2016-07-30 16:53:15 +0200

    Java update

--- json/json-java/src/main/java/bench/TestJava.java
+++ json/json-java/src/main/java/bench/TestJava.java
@@ -8,33 +8,24 @@ public class TestJava {
 
 	@CompiledJson
 	public static class Root {
-		private List<Model> c;
-		public List<Model> getCoordinates() { return c; }
-		public void setCoordinates(List<Model> v) { c = v; }
+		public List<Model> coordinates;
 	}
 
-	@CompiledJson
 	public static class Model {
-		private double x, y, z;
-		public double getX() { return x; }
-		public double getY() { return y; }
-		public double getZ() { return z; }
-		public void setX(double v) { x = v; }
-		public void setY(double v) { y = v; }
-		public void setZ(double v) { z = v; }
+		public double x, y, z;
 	}
-
+	
 	public static void parse(String filename) throws IOException {
 		long start_time = System.currentTimeMillis();
 		FileInputStream fis = new FileInputStream(filename);
 		DslJson<Object> json = new DslJson<Object>();
-		Root result = json.deserialize(Root.class, fis, new byte[8196]);
+		Root result = json.deserialize(Root.class, fis, new byte[65536]);
 		double x = 0, y = 0, z = 0;
-		int total = result.getCoordinates().size();
-		for(Model m : result.getCoordinates()) {
-			x += m.getX();
-			y += m.getY();
-			z += m.getZ();
+		int total = result.coordinates.size();
+		for(Model m : result.coordinates) {
+			x += m.x;
+			y += m.y;
+			z += m.z;
 		}
 		System.out.println(x / total);
 		System.out.println(y / total);
@@ -44,7 +35,7 @@ public class TestJava {
 
 	public static void main(String[] args) throws IOException {
 		// warming
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < 10; i++) {
 			parse("1.json");
 		}
 	}
