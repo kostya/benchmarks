c60c5a0f764594af8e4e0d98dc6486864fdd9826
Hubert Jarosz <marqin.pl@gmail.com>
2016-08-10 12:03:13 +0200

    enable LTO in C, C++ and Rust benchmarks

--- brainfuck2/build.sh
+++ brainfuck2/build.sh
@@ -1,6 +1,6 @@
 crystal build bf.cr --release -o bin_cr
-g++ -O3 -o bin_cpp bf.cpp
-rustc -C opt-level=3 bf.rs -o bin_rs
+g++ -flto -O3 -o bin_cpp bf.cpp
+rustc -C lto -C opt-level=3 bf.rs -o bin_rs
 scalac -optimize bf.scala
 mcs -debug- -optimize+ bf.cs
 javac bf.java
@@ -12,4 +12,3 @@ gdc -o bin_d_gdc -O3 -frelease -finline bf.d
 ldc2 -ofbin_d_ldc -O5 -release -inline bf.d
 nim c -o:bin_nim_clang -d:release --cc:clang --verbosity:0 bf.nim
 nim c -o:bin_nim_gcc -d:release --cc:gcc --verbosity:0 bf.nim
-
