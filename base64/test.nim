dd6bc703dd9db3582e02910a15af3146abde2877
donlzx <donlzx@users.noreply.github.com>
2017-03-18 15:03:46 +0800

    [Nim] Improve base64 encoding performance
    * Fix base64 encoder usage for better performance
    * Replace deprecate function repeatChar() with repeat()

--- base64/test.nim
+++ base64/test.nim
@@ -2,14 +2,16 @@ import base64, times, strutils
 
 let STR_SIZE = 10_000_000
 let TRIES = 100
-let str = strutils.repeatChar(STR_SIZE, 'a')
+let str = strutils.repeat('a', STR_SIZE)
 var str2 = ""
 
 var t = times.epochTime()
 var i = 0
 var s:int64 = 0
 while i < TRIES:
-  str2 = base64.encode(str)
+  # set large line length to avoid adding line breaks,
+  # outputing identical encoded strings as in other languages
+  str2 = base64.encode(str, 2 * STR_SIZE)
   s += len(str2)
   i += 1
 echo("encode: ", s, ", ", formatFloat(times.epochTime() - t, ffDefault, 6))
