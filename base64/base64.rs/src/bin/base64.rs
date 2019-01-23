12c2f68b08c0dcaf4ed5a3b433547c423c29304e
Ivan Tham <pickfire@riseup.net>
2019-01-23 22:31:26 +0800

    Update Rust base64
    Before and after (10 tries)
    
    encode: 133333360, 9.954792112
    decode: 100000000, 10.080134839
    
    encode: 133333360, 1.776409588
    decode: 100000000, 8.38234092

--- base64/base64.rs/src/bin/base64.rs
+++ base64/base64.rs/src/bin/base64.rs
@@ -1,29 +1,36 @@
-extern crate time;
 extern crate base64;
+extern crate time;
 
+use base64::{decode, encode};
 use time::precise_time_ns;
-use base64::{encode, decode};
 
-const STR_SIZE: usize = 10000000;
+const STR_SIZE: usize = 10_000_000;
 const TRIES: usize = 100;
 
 fn main() {
-  let input = vec![b'a'; STR_SIZE];
-  let mut output = String::new();
+    let input = vec![b'a'; STR_SIZE];
+    let mut output = String::new();
 
-  let mut time_start = precise_time_ns();
-  let mut sum = 0;
-  for _ in 0..TRIES {
-    output = encode(&input);
-    sum += output.len();
-  }
-  println!("encode: {}, {}", sum, ((precise_time_ns() - time_start) as f64) / 1e9);
+    let time_start = precise_time_ns();
+    let mut sum = 0;
+    for _ in 0..TRIES {
+        output = encode(&input);
+        sum += output.len();
+    }
+    println!(
+        "encode: {}, {}",
+        sum,
+        ((precise_time_ns() - time_start) as f64) / 1e9
+    );
 
-  sum = 0;
-  time_start = precise_time_ns();
-  for _ in 0..TRIES {
-    sum += decode(&output).unwrap().len();
-  }
-  println!("decode: {}, {}", sum, ((precise_time_ns() - time_start) as f64) / 1e9);
+    let mut sum = 0;
+    let time_start = precise_time_ns();
+    for _ in 0..TRIES {
+        sum += decode(&output).unwrap().len();
+    }
+    println!(
+        "decode: {}, {}",
+        sum,
+        ((precise_time_ns() - time_start) as f64) / 1e9
+    );
 }
-
