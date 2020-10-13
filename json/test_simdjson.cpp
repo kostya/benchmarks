e9ac7361beb7bd951ca0c9e61f71c13f023059ef
Daniel Lemire <lemire@gmail.com>
2020-10-13 19:57:45 -0400

    Switch the simdjson code to On Demand (#272)
    * Updating simdjson to 0.6 API (on demand).
    
    * Making things safer by specifying the commit + removing unnecessary std:: qualifiers.
    
    * Minor changes following review

--- json/test_simdjson.cpp
+++ json/test_simdjson.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <libnotify.hpp>
-#include "simdjson.h"
 #include <sstream>
 #include <unistd.h>
 #include <fstream>
 
-using namespace simdjson;
+#include "simdjson.h"
+
 using namespace std;
 
 struct coordinate_t {
@@ -23,63 +23,52 @@ struct coordinate_t {
   }
 };
 
-string read_file(const string& filename) {
-  ifstream f(filename);
-  if (!f) {
-    return {};
+
+using namespace simdjson;
+using namespace simdjson::builtin;
+
+class on_demand {
+public:
+  bool run(const padded_string &json);
+  coordinate_t my_point{};
+  size_t count{};
+private:
+  ondemand::parser parser{};
+};
+
+bool on_demand::run(const padded_string &json) {
+  count = 0;
+  auto doc = parser.iterate(json);
+  for (ondemand::object point_object : doc["coordinates"]) {
+    my_point.x += double(point_object["x"]);
+    my_point.y += double(point_object["y"]);
+    my_point.z += double(point_object["z"]);
+    count++;
   }
-  return string(istreambuf_iterator<char>(f),
-                istreambuf_iterator<char>());
+  return true;
 }
 
-coordinate_t calc(const string& text) {
-  dom::parser pj(0);
-  auto allocate_error = pj.allocate(text.size()); // allocate memory for parsing up to p.size() bytes
-  if (allocate_error) {
-    cerr << allocate_error << endl;
-    exit(EXIT_FAILURE);
-  }
-
-  auto [doc, error] = pj.parse(text); // do the parsing, return 0 on success
-  if (error) {
-    cerr << error << endl;
-    exit(EXIT_FAILURE);
-  }
-
-  auto x = 0.0, y = 0.0, z = 0.0;
-  auto len = 0;
-
-  for (auto coord : doc["coordinates"]) {
-    double x_coord = coord["x"];
-    x += x_coord;
-
-    double y_coord = coord["y"];
-    y += y_coord;
-
-    double z_coord = coord["z"];
-    z += z_coord;
-
-    len++;
-  }
-
-  return coordinate_t(x / len, y / len, z / len);
+coordinate_t calc(const simdjson::padded_string& json) {
+  on_demand reader;
+  reader.run(json);
+  reader.my_point.x /= reader.count;
+  reader.my_point.y /= reader.count;
+  reader.my_point.z /= reader.count;
+  return reader.my_point;
 }
 
 int main() {
-  auto left = calc("{\"coordinates\":[{\"x\":1.1,\"y\":2.2,\"z\":3.3}]}");
+  auto left = calc("{\"coordinates\":[{\"x\":1.1,\"y\":2.2,\"z\":3.3}]}"_padded); // The _padded suffix creates a simdjson::padded_string instance
   auto right = coordinate_t(1.1, 2.2, 3.3);
   if (left != right) {
     cerr << left << " != " << right << endl;
     exit(EXIT_FAILURE);
   }
-
-  auto text = read_file("/tmp/1.json");
-
+  auto [text, error] = simdjson::padded_string::load("/tmp/1.json");
+  if(error) { cerr << "could not load file" << endl; return EXIT_FAILURE; }
   stringstream ostr;
   ostr << "C++/g++ (simdjson)\t" << getpid();
   notify(ostr.str());
-
   cout << calc(text) << endl;
-
   notify("stop");
 }
