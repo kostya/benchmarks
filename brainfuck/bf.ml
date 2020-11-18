6345a02e0f414fe284cc9a3faf4719e247c2659f
Alexander Slesarev <alex.slesarev@gmail.com>
2020-11-18 15:50:14 -0700

    OCaml tests have been improved.

--- brainfuck/bf.ml
+++ brainfuck/bf.ml
@@ -1,8 +1,25 @@
 type op = Inc of int | Move of int | Print | Loop of op list
 type tape =
-    { data : int array;
-      pos : int;
-    }
+  { data : int array;
+    pos : int;
+  }
+type printer =
+  { sum1 : int;
+    sum2: int;
+    quiet: bool;
+  }
+
+let print p n =
+  if p.quiet then
+    let newP = { p with sum1 = (p.sum1 + n) mod 255 } in
+    { newP with sum2 = (newP.sum1 + newP.sum2) mod 255}
+  else begin
+      print_char (Char.chr n);
+      flush stdout;
+      p
+    end
+
+let get_checksum p = (p.sum2 lsl 8) lor p.sum1
 
 let current t =
   t.data.(t.pos)
@@ -35,22 +52,21 @@ let rec parse (s, acc) =
     | ']' -> (rest, List.rev acc)
     | _ -> parse (rest, acc)
 
-let rec run program t =
+let rec run program t p =
   match program with
-  | [] -> t
+  | [] -> (t, p)
   | Inc d :: ops ->
-      inc d t;
-      run ops t
-  | Move m :: ops -> run ops (move m t)
+     inc d t;
+     run ops t p
+  | Move m :: ops -> run ops (move m t) p
   | Print :: ops ->
-      print_char (Char.chr (current t));
-      flush stdout;
-      run ops t
+     let newP = print p (current t) in
+     run ops t newP
   | Loop loop_code :: ops ->
-     let rec loop t =
-       if current t = 0 then run ops t
-       else loop (run loop_code t)
-     in loop t
+     let rec loop (t, p) =
+       if current t = 0 then run ops t p
+       else loop (run loop_code t p)
+     in loop (t, p)
 
 let read_file filename =
   let s = ref "" in
@@ -72,16 +88,38 @@ let notify msg =
       ~finally:(fun() -> close_out oc)
   with Unix.Unix_error _ -> ()
 
+let verify =
+  let source = {|++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>\
+                ---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.|} in
+  let (_, ops) = parse(source, []) in
+  let p = { sum1 = 0; sum2 = 0; quiet = true } in
+  let (_, pLeft) = run ops { data = [| 0 |]; pos = 0 } p in
+  let left = get_checksum pLeft in
+
+  let s = "Hello World!\n" in
+  let seq = List.init
+              (String.length s)
+              (fun i -> (Char.code (String.get s i))) in
+  let pRight = List.fold_left print p seq in
+  let right = get_checksum pRight in
+  if left != right then begin
+      Printf.eprintf "%d != %d\n" left right;
+      exit 1
+    end
+
 let main =
+  verify;
   match Sys.argv with
   | [| _; filename |] ->
-    let source = read_file filename in
+     let source = read_file filename in
+     let quiet = Sys.getenv_opt "QUIET" in
+     let p = { sum1 = 0; sum2 = 0; quiet = Option.is_some quiet} in
 
-    let pid = Unix.getpid() in
-    notify(Printf.sprintf "OCaml\t%d" pid);
+     notify(Printf.sprintf "OCaml\t%d" (Unix.getpid()));
+     let (_, ops) = parse(source, []) in
+     let (_, newP) = run ops { data = [| 0 |]; pos = 0 } p in
+     notify "stop";
 
-    let (_, ops) = parse(source, []) in
-    ignore(run ops { data = [| 0 |]; pos = 0 });
-
-    notify "stop"
+     if newP.quiet then
+       Printf.printf "Output checksum: %d\n" (get_checksum newP)
   | _ -> exit 1
