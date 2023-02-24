d42b5c79a9f857748f773baf996601b728a36441
Alexander Slesarev <alex.slesarev@gmail.com>
2023-02-24 05:46:55 +0000

    Experimental support for Alpine image.

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -xe
 
 docker_build="docker build docker/ -t benchmarks"
+docker_alpine_build="docker build docker/ -t benchmarks-alpine -f docker/Dockerfile.alpine"
+
 docker_run="docker run -it --rm -v $PWD:/src benchmarks"
+docker_alpine_run="docker run -it --rm -v $PWD:/src benchmarks-alpine"
+
 exit_trap_command=""
 
 tear_down() {
@@ -22,19 +26,31 @@ build() {
     eval "$docker_build"
 }
 
+build_alpine() {
+    eval "$docker_alpine_build"
+}
+
 rebuild() {
     eval "$docker_build --no-cache"
 }
 
-shell() {
+setup_shell() {
     # TODO: apply the proper solution (probably by using the privileged monitor)
     # References (CVE-2020-8694): https://lists.debian.org/debian-lts-announce/2020/12/msg00015.html
     old_mode=$(stat -c "%a" /sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj)
     energy_uj=/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj
     chmod 0444 "$energy_uj"
     add_exit_trap "chmod $old_mode $energy_uj"
+}
 
+shell() {
+    setup_shell
     eval "$docker_run shell"
+}
+
+shell_alpine() {
+    setup_shell
+    eval "$docker_alpine_run shell"
 }
 
 versions() {
@@ -51,6 +67,7 @@ update_apt() {
 
 lint() {
     docker run --rm -i hadolint/hadolint < docker/Dockerfile
+    docker run --rm -i hadolint/hadolint < docker/Dockerfile.alpine
 }
 
 "$@"
